91a9c45edb7b81d701fafa0b5a97d6b9051d8c33
安亚飞 <yafei.an@mtime.com>
2017-12-01 18:26:32 +0800

    添加文件

--- src/main/java/an/test/web/controller/MainController.java
+++ src/main/java/an/test/web/controller/MainController.java
@@ -1,4 +1,11 @@
 package an.test.web.controller;
 
+import java.security.PublicKey;
+
 public class MainController {
+
+    public static void main(String[] args) {
+        System.out.println("This is test!");
+    }
+
 }
